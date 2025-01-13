3d8219c5b83f8450bae5ca54e763869a612c20c3
hyewonp0724@gmail.com <hyewonp0724@gmail.com>
2025-01-14 03:12:08 +0900

    change in main.ts validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,15 @@ import { AppService } from './app.service';
 import { TodoModule } from './todo.module';
 import { AuthModule } from './auth.module';
 import { AuthController } from './interfaces/controller/auth.controller';
-import { AuthService } from './service/auth.service';
+import { AuthService } from '../src/application/service/auth.service';
 import { PollModule } from './poll.module';
-import { PollService } from './service/poll.service';
+import { PollService } from '../src/application/service/poll.service';
 import { PollController } from './interfaces/controller/poll.controller';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import config from './mikro-orm.config';
 import { MovieUserModule } from './movie-user.module';
 import { MovieUserController } from './interfaces/controller/movie-user.controller';
-import { MovieUserService } from 'src/service/movie-user.service';
+import { MovieUserService } from '../src/application/service/movie-user.service';
 
 @Module({
   imports: [MikroOrmModule.forRoot(config), TodoModule, AuthModule, PollModule, MovieUserModule],
